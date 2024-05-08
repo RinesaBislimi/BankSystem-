4363c5d42f6387aebece1ea3d17361c6561a2129
Rinesa Bislimi <118773246+RinesaBislimi@users.noreply.github.com>
2024-05-09 01:40:13 +0200

    Initial commit

---
+++ src/main/java/com/example/BankSystemSpringBoot/BankSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankSystemSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BankSystemSpringBoot/BankSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankSystemSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
